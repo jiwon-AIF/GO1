c7ef8cfadc9359f5c43e4a0de54f7ab0ed167aaa
Jiwon <jwj50364@gmail.com>
2020-04-03 13:20:06 +0900

    cal
    cal

--- calculator9/cal.go
+++ calculator9/cal.go
@@ -11,10 +11,9 @@ import (
 )
 
 var myLogger *log.Logger
+var choice string
 
-func main() {
-	fmt.Println("github setting!!!")
-	fmt.Println("C : 계산기 실행, L : 로또 번호 생성, Q : 종료")
+func main() {    
 
 	reader := bufio.NewReader(os.Stdin) // os 운영체제가 갖고 있는 표준 입력을 통해 읽기용 객체 생성
 	line, _ := reader.ReadString('\n') // 에러 처리 없이 한 줄 읽기
@@ -35,9 +34,12 @@ myLogger.Println("End of Program")
 
 func run() {
 myLogger.Print("Test")
-	for {
-		i := "C"
-if i == "C" {
+
+Loop: 
+fmt.Println("C : 계산기 실행, L : 로또 번호 생성, Q : 종료")
+fmt.Scanf("%v", &choice)
+
+if choice == "C" {
 	fmt.Println("숫자를 입력하세요")
 	myLogger.Println("숫자를 입력하세요")
 
@@ -45,7 +47,7 @@ if i == "C" {
 	line, _ := reader.ReadString('\n')
 	line = strings.TrimSpace(line)
 
-	n1, _ := strconv.ParseFloat(line, 64) // 에러 처리 없이 문자를 숫자로 컨버팅
+	n1, _ := strconv.ParseFloat(line, 64) 
 
 	line, _ = reader.ReadString('\n')
 	line = strings.TrimSpace(line)
@@ -67,48 +69,56 @@ if i == "C" {
 	if line == "+" {
 		fmt.Printf("%.2f + %.2f + %.2f = %.2f", n1, n2, n3, n1+n2+n3)
 		myLogger.Printf("%.2f + %.2f +  %.2f = %.2f", n1, n2, n3, n1+n2+n3) 
+		line, _ = reader.ReadString('\n')
+		line = strings.TrimSpace(line)
+		goto Loop
 	} else if line == "-" {
 		fmt.Printf("%.2f - %.2f - %.2f = %.2f", n1, n2, n3, n1-n2-n3)
 		myLogger.Printf("%.2f - %.2f - %.2f = %.2f", n1, n2, n3, n1-n2-n3)
+		line, _ = reader.ReadString('\n')
+		line = strings.TrimSpace(line)
+		goto Loop
 	} else if line == "*" {
 		fmt.Printf("%.2f * %.2f * %.2f = %.2f", n1, n2, n3, n1*n2*n3)
 		myLogger.Printf("%.2f * %.2f * %.2f = %.2f", n1, n2, n3, n1*n2*n3)
+		line, _ = reader.ReadString('\n')
+		line = strings.TrimSpace(line)
+		goto Loop
 	} else if line == "/" {
 		fmt.Printf("%.2f / %.2f / %.2f = %.2f", n1, n2, n3, n1/n2/n3)
 		myLogger.Printf("%.2f / %.2f / %.2f = %.2f", n1, n2, n3, n1/n2/n3)	
-	} else if line == "q" {
-		break
+		line, _ = reader.ReadString('\n')
+		line = strings.TrimSpace(line)
+		goto Loop
 	} else {
 		fmt.Println("잘못 입력하셨습니다.")
 		myLogger.Println("잘못 입력하셨습니다.")
+		goto Loop
 	}
-	line, _ = reader.ReadString('\n')
-	line = strings.TrimSpace(line)
-
-	if line == "L" {
+}
+    
+	if choice == "L" {
 
 	a := make([]int, 6) // int형 배열 선언
 	fmt.Println("행운의 숫자는 :")
-
-	line, _ = reader.ReadString('\n')
-	line = strings.TrimSpace(line)
-
+ 
 	for i := 0; i < len(a); i++ { //숫자 6개를 뽑기 위한 for문
 		a[i] = rand.Intn(45) + 1 // 1~45 숫자 중 랜덤으로 하나를 뽑아 a[0]~a[6]에 저장
-
+	
 		for j := 0; j < i; j++ { // 중복제거를 위한 for문 : 현재 a[]에 저장된 랜덤숫자와 이전 a[]에 들어간 숫자 비교
 			if a[i] == a[j] {    
 				i--
 			}
 		}
 	}
-	for k := 0; k <= 5; k++ { // 채워진 배열을 출력하기 위한 for문
-			fmt.Println(a[k])
+	for k := 0; k <=5; k++ { // 채워진 배열을 출력하기 위한 for문
+		fmt.Println(a[k])
+		if k == 5 {
+			fmt.Println("입니다.")
+			goto Loop
+		}
+
 	}
-	fmt.Println("입니다.")
-	line, _ = reader.ReadString('\n')
-	line = strings.TrimSpace(line)
-}
+
 }
 	}
-}
